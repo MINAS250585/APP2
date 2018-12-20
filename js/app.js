a8ca1d4a1f51dfc24792ad652a78092aaf223daf
MINA250585 <mina_palacios@hotmail.com>
2018-12-20 09:17:03 -0600

    array curso

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function new_element(){
+    console.log("entro") //
+$("h1").append("<p>JAVA</p>") 
+$("h1").append("<p>JAVA</p>")
+$("h1").append("<p>JAVA</p>") 
+//$("h1").append("<p><img src='img/ss.jpg'><p>")
+$("body").append("<p><img src='img/ss.jpg'><p>")
+
+}
+
+// git init practica
+
+$("#btn_clic").click(function()  { //con # indica el id del elemento
+    console.log("entro2")
+alert("ALERTA AL CLICK");
+
+
+});
+
+
+//Array
+var frutas = ["uvas","platano","fresa","mango"]
+
